f1768f38145e33c73ec6858270f8a4f4fa593f7f
eka <eka@example.com>
2021-05-30 10:36:08 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LS_FundParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
